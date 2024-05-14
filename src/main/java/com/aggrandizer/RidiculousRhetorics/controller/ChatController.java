ae727ad5fba379c46a9beb1f90af69db17fa384f
vlbandara <137267283+vlbandara@users.noreply.github.com>
2024-05-14 15:36:02 +0530

    Update ChatController.java

--- src/main/java/com/aggrandizer/RidiculousRhetorics/controller/ChatController.java
+++ src/main/java/com/aggrandizer/RidiculousRhetorics/controller/ChatController.java
@@ -1,9 +1,18 @@
 package com.aggrandizer.RidiculousRhetorics.controller;
 
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.parser.MapOutputParser;
+import org.springframework.ai.parser.OutputParser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 
 @RestController
 public class ChatController {
@@ -13,9 +22,17 @@ public class ChatController {
         this.chatClient = chatClient;
     }
 
-    @GetMapping("/rhetorics")
-    public String generateRhetorics(@RequestParam(value = "message", defaultValue = "tell me a Random Rhetorical Question") String message) {
-        return chatClient.call(message);
-
+    @GetMapping("/rhetorics/{type}")
+    public Map<String,Object> getRhetorics(@PathVariable String type){
+        String promptMessage = """
+                generate a rhetoric question and answer about {type}. Include in  {format}
+                """;
+        MapOutputParser outputParser = new MapOutputParser();
+        String format = outputParser.getFormat();
+        PromptTemplate promptTemplate = new PromptTemplate(promptMessage, Map.of("type", type, "format", format));
+        Prompt prompt = promptTemplate.create();
+        Generation generation = chatClient.call(prompt).getResult();
+        return outputParser.parse(generation.getOutput().getContent());
     }
+
 }
